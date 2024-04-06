77f6f3dde3ae304d0b0accfca26a4a189e102b42
ChidhvilasTanay <73900317+ChidhvilasTanay@users.noreply.github.com>
2024-04-06 12:52:34 +0530

    included streamlit app

---
+++ langchain_helper.py
@@ -0,0 +1,22 @@
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+from langchain.prompts.prompt import PromptTemplate
+from langchain.chains import LLMChain
+
+load_dotenv()
+
+prompt_template_name = PromptTemplate(
+    input_variables=['animal_type', 'pet_color'],
+    template = "I have a pet {animal_type}, it is {pet_color} in color, and I need a cool name for it, suggest me five good ones."
+)
+
+
+def generate_pet_name(animal_type, pet_color):
+    llm = OpenAI(temperature=0.7)
+    
+    
+    name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key='pet_name')
+
+    response = name_chain.invoke({'animal_type':animal_type, 'pet_color':pet_color})
+
+    return response
--- main.py
+++ main.py
@@ -1,25 +1,13 @@
-from langchain_openai import OpenAI
-from dotenv import load_dotenv
-from langchain.prompts.prompt import PromptTemplate
-from langchain.chains import LLMChain
+import langchain_helper as lch
+import streamlit as st
 
-load_dotenv()
+st.title("pet name finder")
 
-prompt_template_name = PromptTemplate(
-    input_variables=['animal_type', 'pet_color'],
-    template = "I have a pet {animal_type}, it is {pet_color} in color, and I need a cool name for it, suggest me five good ones."
-)
+user_animal_type = st.sidebar.selectbox("which animal is your pet?", ("cat","dog", "parrot", "fish"))
 
+if user_animal_type:
+    user_pet_color = st.sidebar.text_area(label=f"what color is your {user_animal_type}?", max_chars=15)
 
-def generate_pet_name(animal_type, pet_color):
-    llm = OpenAI(temperature=0.7)
-    
-    
-    name_chain = LLMChain(llm=llm, prompt=prompt_template_name)
-
-    response = name_chain.invoke({'animal_type':animal_type, 'pet_color':pet_color})
-
-    return response
-
-if (__name__ == "__main__"):
-    print(generate_pet_name("cat", "pink"))
+if user_pet_color:
+    response = lch.generate_pet_name(user_animal_type, user_pet_color)
+    st.text(response['pet_name'])
